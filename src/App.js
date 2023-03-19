181854b4a4041835719dc8a96b83ab41bd4d9e1e
Abhishek <abhishekanand5980@gmail.com>
2023-03-19 22:12:17 +0530

    Counter Project Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
+ import {useState} from "react";
 import './App.css';
-
+import './App.css';
 function App() {
+
+   
+  const[Count, setCount]=useState(0);
+
+  function decreaseHandler(){
+    setCount(Count-1);
+  }
+
+  function increaseHandler(){
+    setCount(Count+1);
+  }
+
+  function resetHandler(){
+    setCount(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="heading">Increment & Decrement</div>
+
+       <div className="btn">
+        <button onClick={decreaseHandler} className="decrement">-</button>
+        <div className='count'>{Count}</div>
+        <button onClick={increaseHandler} className="increment ">+</button>
+       </div>
+
+       <div onClick={resetHandler} className="reset">Reset</div>
     </div>
   );
 }
